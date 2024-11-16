5cc9ef61a1e6a830a85c0a0146609b99443bdd39
khalifahnur <khalifahnur1095@gmail.com>
2024-11-16 13:29:12 +0300

    testimonial placeholder data

---
+++ src/components/landing/Testimonial.ts
@@ -0,0 +1,21 @@
+ const testimonials = [
+    {
+      name: "Jony Lauren",
+      image: "/user.png",
+      rating: 5,
+      text: "I recently dined at The Gourmet Haven and was absolutely blown away by the experience. From the moment I walked in, I was greeted warmly by the staff, and the ambiance was both elegant and cozy."
+    },
+    {
+      name: "Maria Jason",
+      image: "/user.png",
+      rating: 5,
+      text: "My visit to The Culinary Delight was nothing short of extraordinary. As soon as I stepped through the door, I was welcomed with a smile, and the atmosphere struck a perfect balance between sophistication and comfort."
+    },
+    {
+      name: "Sarah Lackin",
+      image: "/user.png",
+      rating: 5,
+      text: "Dining at The Epicurean Escape was an unforgettable experience. The staff greeted me with genuine warmth, and the setting was a harmonious blend of elegance and homeliness, making me feel right at ease."
+    }
+  ];
+  export default testimonials;
