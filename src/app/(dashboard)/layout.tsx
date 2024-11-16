1b56039176133e0e1a1bf5d392a596c2022d9420
khalifahnur <khalifahnur1095@gmail.com>
2024-11-16 14:16:53 +0300

    ref:sidebartrigger

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/Sidebar/app-sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
