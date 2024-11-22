d0ab4a91eb51299b7c1e7369dc2d044eb3247606
khalifahnur <khalifahnur1095@gmail.com>
2024-11-22 18:03:02 +0300

    ref:changed closing and oening icon of menu

--- src/components/Sidebar/app-sidebar.tsx
+++ src/components/Sidebar/app-sidebar.tsx
@@ -1,23 +1,30 @@
+"use client"
+
 import {
   Sidebar,
   SidebarHeader,
+  SidebarSeparator,
   SidebarTrigger,
+  useSidebar,
 } from "@/components/ui/sidebar";
 
 import SideBarFooter from "./sidebar-footer";
 import { SideBarContent } from "./side-barcontent";
 
 export function AppSidebar() {
+  const { toggleSidebar, state } = useSidebar();
+
   return (
     <Sidebar variant="floating" collapsible="icon">
       <SidebarHeader>
-        <div className="flex flex-row justify-between items-center space-x-10">
-          <span>Swiftab</span>
-          <div className="p-4">
+        <div className="flex flex-row justify-between items-center px-0 h-16">
+          {state === "expanded" && <span className="px-5">Swiftab</span>}
+          <div className="items-center flex">
             <SidebarTrigger />
           </div>
         </div>
       </SidebarHeader>
+      <SidebarSeparator />
       <SideBarContent />
       <SideBarFooter />
     </Sidebar>
