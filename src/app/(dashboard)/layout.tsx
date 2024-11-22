916c9aad9b6c77c18786e005668bf649e33b1d6b
khalifahnur <khalifahnur1095@gmail.com>
2024-11-22 17:53:58 +0300

    ref:changed dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,23 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/Sidebar/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/Sidebar/app-sidebar"
+import { DashboardContent } from "@/components/pages/Container"
 
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <SidebarProvider>
       <div className="flex min-h-screen w-full">
         <AppSidebar />
         <main className="flex-1 flex flex-col">
-          <div className="flex-1 w-full overflow-auto p-4">{children}</div>
+          <div className="flex-1 w-full overflow-auto p-4">
+            <DashboardContent>{children}</DashboardContent>
+          </div>
         </main>
       </div>
     </SidebarProvider>
-  );
+  )
 }
+
