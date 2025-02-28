c7391824fcc2e3babde718078403bdf37707a02a
khalifahnur <khalifahnur1095@gmail.com>
2025-02-28 09:40:48 +0300

    sidebar

--- src/components/Sidebar/app-sidebar.tsx
+++ src/components/Sidebar/app-sidebar.tsx
@@ -10,21 +10,53 @@ import {
 
 import SideBarFooter from "./sidebar-footer";
 import { SideBarContent } from "./side-barcontent";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAdminInfo } from "@/hooks/authhook/authhooks";
+import { FullScreenLoader } from "../Loading/FullScreen";
 
 export function AppSidebar() {
   const { state } = useSidebar();
 
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["adminInfo"],
+    queryFn: fetchAdminInfo,
+  });
+
+  if (isLoading) {
+    return <FullScreenLoader />;
+  }
+
+  if (error) {
+    return <div>Error fetching admin info: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>No data available</div>;
+  }
+
+  const { restaurantName, image } = data;
+
   return (
     <Sidebar variant="floating" collapsible="icon">
-      <SidebarHeader>
+      <SidebarHeader
+        style={{
+          backgroundImage: image ? `url(${image})` : "none",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <div className="flex flex-row justify-between items-center px-0 h-16">
-          {state === "expanded" && <span className="px-5">Swiftab</span>}
+          {state === "expanded" && (
+            <span className="text-center font-semibold text-sm text-white hover:text-primary/50 transition-all duration-300">
+              {restaurantName ? restaurantName : "Swiftab"}
+            </span>
+          )}
           <div className="items-center flex">
             <SidebarTrigger />
           </div>
         </div>
       </SidebarHeader>
-      <SidebarSeparator />
+      <SidebarSeparator className="mt-2" />
       <SideBarContent />
       <SideBarFooter />
     </Sidebar>
