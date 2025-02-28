e79d87e9865929c5b3f94612550ae3dfdb423e89
khalifahnur <khalifahnur1095@gmail.com>
2025-02-28 09:38:49 +0300

    added :grid timeline

--- src/app/(dashboard)/tables/grid/page.tsx
+++ src/app/(dashboard)/tables/grid/page.tsx
@@ -1,12 +1,14 @@
+import Container from "@/components/pages/table/grid/Container";
 import React from "react";
 
 export default function Grid() {
   return (
-    <div className="grid-background flex items-center justify-center h-screen">
-      <div className="content bg-white rounded-lg shadow-lg p-8">
-        <h1 className="text-2xl font-bold">Your Content Here</h1>
-        <p>This is your page content over a grid-like background.</p>
-      </div>
-    </div>
+    // <div className="grid-background flex items-center justify-center h-screen">
+    //   <div className="content bg-white rounded-lg shadow-lg p-8">
+    //     <h1 className="text-2xl font-bold">Your Content Here</h1>
+    //     <p>This is your page content over a grid-like background.</p>
+    //   </div>
+    // </div>
+      <Container />
   );
 }
