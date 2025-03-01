fc2bda980caf84d01e6c6d6bab1b53fbce355fff
khalifahnur <khalifahnur1095@gmail.com>
2025-03-01 10:53:54 +0300

    ref: changed the layout of tabs and then added static header component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,9 @@
+import { DashboardLayout } from "@/components/pages/DashboardLayout";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/Sidebar/app-sidebar";
-import { DashboardContent } from "@/components/pages/Container";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function DashboardLayout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
@@ -18,14 +17,7 @@ export default async function DashboardLayout({
 
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        <AppSidebar />
-        <main className="flex-1 flex flex-col">
-          <div className="flex-1 w-full overflow-auto p-4">
-            <DashboardContent>{children}</DashboardContent>
-          </div>
-        </main>
-      </div>
+      <DashboardLayout>{children}</DashboardLayout>
     </SidebarProvider>
   );
 }
