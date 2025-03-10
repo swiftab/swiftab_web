9c9420684f59e18b206316a8a85f5b069091ac54
khalifahnur <khalifahnur1095@gmail.com>
2025-03-10 21:42:08 +0300

    fix:cors restriction

--- next.config.ts
+++ next.config.ts
@@ -4,23 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://server-production-2ee7.up.railway.app",
-          },
-        ],
-      },
-    ];
-  },
   experimental: {
     serverActions: {
       allowedOrigins: [
